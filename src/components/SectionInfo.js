074571bc081e2b53b10ee57c96b39ca1576c1283
ksawery <ksawery.karczewski@mediamonks.com>
2022-04-24 20:48:00 +0200

    navbar on home - to fix

--- src/components/SectionInfo.js
+++ src/components/SectionInfo.js
@@ -14,14 +14,16 @@ export default class SectionInfo extends Component {
                 {t('home:section-info.block', { returnObjects: true }).map((item) => {
                     return (
                         <div className={styles['info-block']} key={item.title}>
-                            <img className={styles['info-block__img']} src={item.img} alt="text" width="100%" height="100%"></img>
-                            <h2 className={styles['info-block__title']}>{item.title}</h2>
-                            <Button className="info-block__button ir" href={item.href}>
-                                {item.title}
-                                <svg className={styles['info-block__button-icon']} viewBox="0 0 26 9">
-                                    <path d="M0.211,3.435 L17.800,3.338 C17.800,2.038 17.795,1.288 17.749,0.133 C20.535,1.604 22.969,2.870 25.789,4.137 C23.030,5.495 20.615,6.731 17.749,8.142 C17.889,6.886 17.800,6.319 17.800,4.937 L0.211,4.746 L0.211,3.435 Z"></path>
-                                </svg>
-                            </Button>
+                            <div className={styles['info-block__wrapper']}>
+                                <img className={styles['info-block__img']} src={item.img} alt="text" width="100%" height="100%"></img>
+                                <h2 className={styles['info-block__title']}>{item.title}</h2>
+                                <Button className="info-block__button ir" href={item.href}>
+                                    {item.title}
+                                    <svg className={styles['info-block__button-icon']} viewBox="0 0 26 9">
+                                        <path d="M0.211,3.435 L17.800,3.338 C17.800,2.038 17.795,1.288 17.749,0.133 C20.535,1.604 22.969,2.870 25.789,4.137 C23.030,5.495 20.615,6.731 17.749,8.142 C17.889,6.886 17.800,6.319 17.800,4.937 L0.211,4.746 L0.211,3.435 Z"></path>
+                                    </svg>
+                                </Button>
+                            </div>
                         </div>
                     );
                 })}
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,25 @@ import { withTranslation, getTranslation } from 'utils/translations/i18n';
 
 //import Button from 'components/Button';
 import Banner from 'components/Banner';
+import NavigationBar from 'components/NavigationBar';
 import VideoPlayer from 'components/VideoPlayer';
 
 import SectionAbout from 'components/SectionAbout';
 import SectionShowcase from 'components/SectionShowcase';
 import SectionInfo from 'components/SectionInfo';
 
+import { resizeManager } from '@superherocheesecake/next-resize-manager';
+
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          isDesktop: false
+        };
+
+        this.detectWindowWidth = this._detectWindowWidth.bind(this);
+    }
+
     el = createRef();
 
     _mousePosition = {
@@ -21,6 +33,7 @@ class Home extends Component {
 
     componentDidMount() {
         this._setupEventListener();
+        this._resize();
     }
 
     componentWillUnmount() {
@@ -29,6 +42,7 @@ class Home extends Component {
 
     render() {
         const { t } = this.props;
+        const isDesktop = this.state.isDesktop;
 
         return (
             <div ref={this.el}>
@@ -37,13 +51,18 @@ class Home extends Component {
                 </Head>
 
                 <Banner />
+                {isDesktop && 
+                    <NavigationBar t={t}  />
+                }
+                {/* {isDesktop ? (
+                <NavigationBar t={t}  />
+                        ) : (
+                <span>not wide</span>
+                )} */}
                 <VideoPlayer />
                 <SectionAbout t={t} />
                 <SectionShowcase t={t} />
                 <SectionInfo t={t} />
-
-                {/* <h1>{t('home:heading')}</h1> */}
-
 
                 {/* <Button className={styles.cta} onClick={this._handleCtaClick}>
                     {t('home:cta')}
@@ -58,12 +77,32 @@ class Home extends Component {
         );
     }
 
-    _removeEventListener() {
-        window.removeEventListener('click', this._handleWindowClick);
-    }
-
     _setupEventListener() {
         window.addEventListener('click', this._handleWindowClick);
+
+        resizeManager.addEventListener('resize', this._resizeHandler);
+        resizeManager.addEventListener('resize:complete', this._resizeHandler);
+        //console.log(window.innerWidth)
+    }
+
+    _removeEventListener() {
+        window.removeEventListener('click', this._handleWindowClick);
+
+        resizeManager.removeEventListener('resize', this._resizeHandler);
+        resizeManager.removeEventListener('resize:complete', this._resizeHandler);
+    }
+
+    _resize() {
+        this._detectWindowWidth();
+    }
+
+    _resizeHandler = () => {
+        this._resize();
+    }
+
+    _detectWindowWidth() {
+        this.setState({ isDesktop: window.innerWidth > 1024 });
+        console.log(window.innerWidth)
     }
 
     _updateBackgroundColor() {
